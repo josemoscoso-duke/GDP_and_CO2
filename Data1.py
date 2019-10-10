6c9b3904d65c0a11f2a93d8bced04753e4c1c67b
Jose Moscoso <jose.moscoso@duke.edu>
2019-10-10 12:39:17 -0400

    download data set

--- Data1.py
+++ Data1.py
@@ -1,3 +1,4 @@
-import urllib2 
+import urllib.request
 
-data = urllib2.urlopen(https://github.com/nickeubank/MIDS_Data/raw/master/World_Development_Indicators/raw_WDI_Data_csv.zip)
+url = 'https://github.com/nickeubank/MIDS_Data/raw/master/World_Development_Indicators/raw_WDI_Data_csv.zip'
+urllib.request.urlretrieve(url, 'E:/Jose_Moscoso/Duke/GitHub/GDP_and_CO2/raw_WDI')
